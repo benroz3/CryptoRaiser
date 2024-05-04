3a84bb4407e5d53b367f3ac7f8d2407df971c4bd
benroz3 <108490385+benroz3@users.noreply.github.com>
2024-05-04 23:15:57 +0300

    fix unused image

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,7 +3,6 @@ import dashboard from './dashboard.svg';
 import logout from './logout.svg';
 import profile from './profile.svg';
 import sun from './sun.svg';
-import withdraw from './withdraw.svg';
 import tagType from './type.svg';
 import search from './search.svg';
 import menu from './menu.svg';
